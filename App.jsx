e43d4888139643d00b9fcd61de96bc49ae0b14d7
Tom Bryden <tombryden@me.com>
2022-03-03 16:54:23 +0000

    Signup linked to backend using axios

--- App.jsx
+++ App.jsx
@@ -1,8 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider as PaperProvider } from "react-native-paper";
+import axios from "axios";
 import Main from "./Main";
 
 export default function App() {
+  // set default axios url to the api
+  axios.defaults.baseURL = "http://localhost:3333/api/1.0.0";
+
   return (
     <PaperProvider>
       <NavigationContainer>
--- Screens/Signup.jsx
+++ Screens/Signup.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { Button, Snackbar, TextInput } from "react-native-paper";
+import axios from "axios";
 
 function Signup() {
+  // Input field states
   const [first, setFirst] = useState("");
   const [surname, setSurname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  //Loading sign up request
+  const [loading, setLoading] = useState(false);
+
+  //snack bar states
+  const [snackText, setSnackText] = useState("");
+  const [snackVisible, setSnackVisible] = useState(false);
 
   return (
     <View style={styles.container}>
@@ -40,9 +49,32 @@ function Signup() {
         onChangeText={(text) => setPassword(text)}
       />
 
-      <Button style={styles.input} mode="outlined">
+      <Button
+        style={styles.input}
+        mode="outlined"
+        loading={loading}
+        onPress={() => {
+          signUp(
+            first,
+            surname,
+            email,
+            password,
+            setSnackText,
+            setSnackVisible,
+            setLoading
+          );
+        }}
+      >
         Sign up
       </Button>
+      <Snackbar
+        visible={snackVisible}
+        onDismiss={() => {
+          setSnackVisible(false);
+        }}
+      >
+        {snackText}
+      </Snackbar>
     </View>
   );
 }
@@ -60,5 +92,43 @@ const styles = StyleSheet.create({
     width: "100%",
   },
 });
+
+// Signup function
+const signUp = (
+  first,
+  surname,
+  email,
+  password,
+  setSnackText,
+  setSnackVisible,
+  setLoading
+) => {
+  setLoading(true);
+
+  // create sign up request object
+  const signUpRequest = {
+    first_name: first,
+    last_name: surname,
+    email,
+    password,
+  };
+
+  // hit user endpoint to create new account with specified info
+  return axios
+    .post("/user", signUpRequest)
+    .then((response) => {
+      console.log(response);
+      setSnackText("Successfully signed up");
+      setSnackVisible(true);
+    })
+    .catch((error) => {
+      console.log(error);
+      setSnackText("Something went wrong");
+      setSnackVisible(true);
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+};
 
 export default Signup;
