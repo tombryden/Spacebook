1f03dfbfc67aeb9554677cdc6f41e63109625a4a
Tom Bryden <tombryden@me.com>
2022-03-03 17:08:11 +0000

    Login linked to backend

--- Screens/Login.jsx
+++ Screens/Login.jsx
@@ -1,10 +1,19 @@
+import axios from "axios";
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { Button, Snackbar, TextInput } from "react-native-paper";
 
 function Login({ navigation }) {
+  // Input states
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  // Login request loading
+  const [loading, setLoading] = useState(false);
+
+  // Snackbar states
+  const [snackText, setSnackText] = useState("");
+  const [snackVisible, setSnackVisible] = useState(false);
 
   return (
     <View style={styles.container}>
@@ -25,7 +34,14 @@ function Login({ navigation }) {
         onChangeText={(text) => setPassword(text)}
       />
 
-      <Button style={styles.input} mode="outlined">
+      <Button
+        style={styles.input}
+        mode="outlined"
+        loading={loading}
+        onPress={() => {
+          login(email, password, setSnackText, setSnackVisible, setLoading);
+        }}
+      >
         Login
       </Button>
 
@@ -38,6 +54,15 @@ function Login({ navigation }) {
       >
         Create an account
       </Button>
+
+      <Snackbar
+        visible={snackVisible}
+        onDismiss={() => {
+          setSnackVisible(false);
+        }}
+      >
+        {snackText}
+      </Snackbar>
     </View>
   );
 }
@@ -55,5 +80,40 @@ const styles = StyleSheet.create({
     width: "100%",
   },
 });
+
+// Login function
+const login = (email, password, setSnackText, setSnackVisible, setLoading) => {
+  // Set login request loading
+  setLoading(true);
+
+  // Create login reqeust object
+  const loginObj = { email, password };
+
+  // Send request to backend
+  return axios
+    .post("/login", loginObj)
+    .then((response) => {
+      console.log(response);
+
+      // Success
+      setSnackText("Successfully logged in");
+      setSnackVisible(true);
+    })
+    .catch((error) => {
+      console.log(error);
+
+      // Invalid email/pwd
+      if (error.response.status === 400) {
+        setSnackText("Invalid email or password");
+        setSnackVisible(true);
+      } else {
+        setSnackText("An internal error occured. Try again later");
+        setSnackVisible(true);
+      }
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+};
 
 export default Login;
