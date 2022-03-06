26cbe3aac14cd2b5bd2faaa2c4ee004155254d21
Tom Bryden <tombryden@me.com>
2022-03-06 21:34:44 +0000

    Added create post functionality

--- Screens/Profile.jsx
+++ Screens/Profile.jsx
@@ -2,25 +2,41 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Avatar, Button } from "react-native-paper";
+import { Avatar, Button, Snackbar, TextInput } from "react-native-paper";
 
 function Profile() {
+  // states for userid/token
+  const [userid, setUserID] = useState("");
+  const [sessionToken, setSessionToken] = useState("");
+
   // states for profile
   const [fullName, setFullName] = useState("");
+
+  // state for posts
+  const [postText, setPostText] = useState("");
+  const [postLoading, setPostLoading] = useState(false);
+
+  // states for snackbar
+  const [snackText, setSnackText] = useState("");
+  const [snackVisible, setSnackVisible] = useState(false);
 
   // get user info - /user on component mount
   useEffect(async () => {
     // get asyncstorage user_id and session_token
     try {
-      const userid = await AsyncStorage.getItem("@user_id");
-      const sessionToken = await AsyncStorage.getItem("@session_token");
+      const auserid = await AsyncStorage.getItem("@user_id");
+      const asessionToken = await AsyncStorage.getItem("@session_token");
 
-      if (userid !== null && sessionToken !== null) {
+      // set states for userid/token to use later
+      setUserID(auserid);
+      setSessionToken(asessionToken);
+
+      if (auserid !== null && asessionToken !== null) {
         // value previously stored
-        console.log(`${userid} | ${sessionToken}`);
+        console.log(`${auserid} | ${asessionToken}`);
 
         // get user info and update states
-        getUserInfo(userid, sessionToken, setFullName);
+        getUserInfo(auserid, asessionToken, setFullName);
       } else {
         // reset and kick back to login
       }
@@ -30,22 +46,62 @@ function Profile() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      {/* profileContainer view contains profile avatar, edit profile button, friends button */}
-      <View style={styles.profileContainer}>
-        <Avatar.Image size={120} />
-        <Text style={styles.nameText}>{fullName}</Text>
-        {/* container for edit profile / friends */}
-        <View style={styles.buttonContainer}>
-          <Button icon="account-edit">Edit Profile</Button>
-          <Button icon="account-group">Friends</Button>
+    <>
+      <View style={styles.container}>
+        {/* profileContainer view contains profile avatar, edit profile button, friends button */}
+        <View style={styles.profileContainer}>
+          <Avatar.Image size={120} />
+          <Text style={styles.nameText}>{fullName}</Text>
+          {/* container for edit profile / friends */}
+          <View style={styles.buttonContainer}>
+            <Button icon="account-edit">Edit Profile</Button>
+            <Button icon="account-group">Friends</Button>
+          </View>
+        </View>
+
+        <View style={styles.postContainer}>
+          <Text style={styles.postText}>Posts</Text>
+          <TextInput
+            mode="outlined"
+            placeholder="Create a new post"
+            right={<TextInput.Affix text="/50" />}
+            multiline
+            numberOfLines={4}
+            onChangeText={(post) => {
+              setPostText(post);
+            }}
+            value={postText}
+          />
+          <Button
+            mode="contained"
+            loading={postLoading}
+            onPress={() => {
+              createNewPost(
+                userid,
+                sessionToken,
+                postText,
+                setSnackText,
+                setSnackVisible,
+                setPostText,
+                setPostLoading
+              );
+            }}
+          >
+            Post
+          </Button>
         </View>
       </View>
 
-      <View style={styles.postContainer}>
-        <Text style={styles.postText}>Posts</Text>
-      </View>
-    </View>
+      {/* snackbar outside container as causes display issue */}
+      <Snackbar
+        visible={snackVisible}
+        onDismiss={() => {
+          setSnackVisible(false);
+        }}
+      >
+        {snackText}
+      </Snackbar>
+    </>
   );
 }
 
@@ -73,7 +129,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "flex-start",
   },
-  postContainer: {},
+  postContainer: { flex: 1, gap: "5px" },
   postText: {
     fontSize: "25px",
   },
@@ -92,6 +148,52 @@ function getUserInfo(userid, token, setFullName) {
     })
     .catch((error) => {
       console.log(error);
+    });
+}
+
+function createNewPost(
+  userid,
+  token,
+  text,
+  setSnackText,
+  setSnackVisible,
+  setPostText,
+  setPostLoading
+) {
+  setPostLoading(true);
+
+  console.log(`${userid} | ${token}`);
+
+  return axios
+    .post(
+      `/user/${userid}/post`,
+      { text },
+      {
+        headers: { "X-Authorization": token },
+      }
+    )
+    .then((response) => {
+      // success - refresh posts with notification and reset create a new post input
+      console.log(response);
+      setSnackText("Post created");
+      setSnackVisible(true);
+      setPostText("");
+    })
+    .catch((error) => {
+      console.log(error);
+      // bad request
+      if (error.response.status === 400) {
+        setSnackText("Something is wrong with your message");
+        setSnackVisible(true);
+      } else if (error.response.status === 401) {
+        // reset and kick back to login - unauthorised
+      } else {
+        setSnackText("An internal error occured. Try again later");
+        setSnackVisible(true);
+      }
+    })
+    .finally(() => {
+      setPostLoading(false);
     });
 }
 
