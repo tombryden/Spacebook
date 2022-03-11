a7b88a17ddf47c3ca40a8a3e2be3fc8260a8723d
Tom Bryden <tombryden@me.com>
2022-03-11 00:43:15 +0000

    Update post functionality

--- Screens/EditPost.jsx
+++ Screens/EditPost.jsx
@@ -23,6 +23,10 @@ function EditPost({ route, navigation }) {
   const [snackText, setSnackText] = useState("");
   const [snackVisible, setSnackVisible] = useState(false);
 
+  // state for loading buttons
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [deleteLoading, setDeleteLoading] = useState(false);
+
   // on mount update post text to contain passed text
   // useEffect(() => {
   //   const { text, postid, userid, token } = route.params;
@@ -50,13 +54,34 @@ function EditPost({ route, navigation }) {
         }}
         value={postText}
       />
-      <Button mode="contained" style={styles.btnConfirm}>
+      <Button
+        mode="contained"
+        style={styles.btnConfirm}
+        loading={confirmLoading}
+        onPress={() => {
+          updatePost(
+            userid,
+            postid,
+            token,
+            navigation,
+            setSnackText,
+            setSnackVisible,
+            getUserPosts,
+            profileUserID,
+            setPosts,
+            setAllPostsLoading,
+            postText,
+            setConfirmLoading
+          );
+        }}
+      >
         Confirm
       </Button>
       <Button
         mode="contained"
         style={styles.btnDelete}
         color="darkred"
+        loading={deleteLoading}
         onPress={() => {
           deletePost(
             userid,
@@ -68,7 +93,8 @@ function EditPost({ route, navigation }) {
             getUserPosts,
             profileUserID,
             setPosts,
-            setAllPostsLoading
+            setAllPostsLoading,
+            setDeleteLoading
           );
         }}
       >
@@ -104,8 +130,11 @@ function deletePost(
   getUserPosts,
   profileUserID,
   setPosts,
-  setAllPostsLoading
+  setAllPostsLoading,
+  setDeleteLoading
 ) {
+  setDeleteLoading(true);
+
   return axios
     .delete(`/user/${userid}/post/${postid}`, {
       headers: { "X-Authorization": token },
@@ -133,6 +162,64 @@ function deletePost(
         setSnackText("Internal error. Try again later");
         setSnackVisible(true);
       }
+    })
+    .finally(() => {
+      setDeleteLoading(false);
+    });
+}
+
+function updatePost(
+  userid,
+  postid,
+  token,
+  navigation,
+  setSnackText,
+  setSnackVisible,
+  getUserPosts,
+  profileUserID,
+  setPosts,
+  setAllPostsLoading,
+  postText,
+  setConfirmLoading
+) {
+  setConfirmLoading(true);
+
+  return axios
+    .patch(
+      `/user/${userid}/post/${postid}`,
+      { text: postText },
+      { headers: { "X-Authorization": token } }
+    )
+    .then(() => {
+      // success
+      // success - refresh posts and navigate to profile
+      getUserPosts(profileUserID, token, setPosts, setAllPostsLoading);
+
+      navigation.goBack();
+    })
+    .catch((err) => {
+      const { status } = err.response;
+      if (status === 400) {
+        // cant happen
+        setSnackText("Internal error. Try again later");
+        setSnackVisible(true);
+      } else if (status === 401) {
+        // unauth
+        navigation.navigate("Login");
+      } else if (status === 403) {
+        // cant happen
+        setSnackText("You can only edit your own posts");
+        setSnackVisible(true);
+      } else if (status === 404) {
+        setSnackText("Post not found");
+        setSnackVisible(true);
+      } else if (status === 500) {
+        setSnackText("Internal error. Try again later");
+        setSnackVisible(true);
+      }
+    })
+    .finally(() => {
+      setConfirmLoading(false);
     });
 }
 
